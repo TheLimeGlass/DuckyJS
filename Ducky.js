c0a831b502211b939e17115839cdb47423544568
BaeFell <nfell2009@live.co.uk>
2016-06-14 00:26:16 +0100

    Sorting out main class + brand new parser!

--- Ducky.js
+++ Ducky.js
@@ -2,21 +2,26 @@
 var Discord = require("discord.js");
 var bot = new Discord.Client();
 var masters = ["91356983042539520", "128194687801622528"];
+var baemasters = ["128194687801622528"];
 var myself = "171294847033016320";
 
-delete exports.triggers;
-delete exports.callbacks;
-delete exports.script2trigger;
-delete exports.mastercmd;
-delete exports.notifyusers;
-delete exports.messagelog;
-delete exports.helpmenu;
+// 0 = running
+// 1 = updating
+// 2 = maintance
+// 3 = fatal error
+exports.modes = ["running", "updating", "maintance", "fatal error"];
+exports.mode = 0;
+
+exports.picturebae = [];
+exports.picturebae.push("155112606661607425");
+exports.autokick = ["100382580146196480", "138441986314207232"];
 exports.triggers = [];
 exports.callbacks = [];
 exports.script2trigger = [];
 exports.mastercmd = [];
 exports.notifyusers = [];
 exports.messagelog = [];
+exports.includes = [];
 
 
 exports.contains = function(base, input) {
@@ -24,6 +29,10 @@ exports.contains = function(base, input) {
 }
 exports.isMaster = function(master) {
 	return isMaster(master);
+}
+
+exports.isBaeMaster = function(master){
+	return isMaster(master, true);
 }
 
 function contains(base, input) {
@@ -46,7 +55,10 @@ function contains(base, input) {
 	}
 }
 
-function isMaster(master) {
+function isMaster(master, baemaster) {
+	if(baemaster) {
+		return contains(baemasters, master);
+	}
 	return contains(masters, master)
 }
 
@@ -66,7 +78,7 @@ function stopMyself(id) {
 
 
 var currentCommandScript;
-function registerCommand(trigger, callback, isMaster) {
+function registerCommand(trigger, callback, isMaster, includes) {
 	if(contains(exports.triggers, trigger)) {
 		var key = -1;
 		for(var t in exports.triggers) {
@@ -78,27 +90,33 @@ function registerCommand(trigger, callback, isMaster) {
 		if(key < 0) {
 			return;
 		}
-		// Is there any need to reset the trigger? Doesn't change
-		//exports.triggers[key] = trigger;
 		exports.callbacks[key] = callback;
-		// No point adding extra args for the command script. It's already set and won't/can't change
-		//exports.script2trigger[key] = currentCommandScript;
 		exports.mastercmd[key] = isMaster;
+		exports.includes.push(includes);
 	} else {
 		exports.triggers.push(trigger);
 		exports.callbacks.push(callback);
 		exports.script2trigger.push(currentCommandScript);
 		exports.mastercmd.push(isMaster);
+		exports.includes.push(includes);
 	}
 }
 
-exports.registerCommand = function(trigger, callback, isMaster, aliases) {
+exports.registerCommand = function(trigger, callback, isMaster, aliases, includes) {
 	isMaster = isMaster || false;
-	registerCommand(trigger, callback, isMaster);
+	includes = includes || false;
+	if(trigger instanceof Array) {
+		for(var t in trigger) {
+			var trig = trigger[t];
+			registerCommand(trig, callback, isMaster, includes)
+		}
+	} else {
+		registerCommand(trigger, callback, isMaster);
+	}
 	if(aliases != null && aliases instanceof Array) {
 		for(var i in aliases) {
 			var alias = aliases[i];
-			registerCommand(alias, callback, isMaster);
+			registerCommand(alias, callback, isMaster, includes);
 		}
 	}
 }
@@ -122,16 +140,10 @@ function isInt(value) {
   return (x | 0) === x;
 }
 
-/*exports.unloadScript = function(path, full) {
-	delete require.cache[path];
-}*/
-
-//var commands = require("./loader/commands.js");
 var fs = require('fs');
 var files = fs.readdirSync('./commands/');
 for (var i in files) {
 	currentCommandScript = files[i];
-  	//var definition = require('./commands/' + files[i]);
   	exports.loadScript('./commands/' + files[i]);
   	console.log('[info] Command script loaded: ' + files[i]);
 }
@@ -139,14 +151,12 @@ for (var i in files) {
 files = fs.readdirSync('./modules/');
 for (var i in files) {
 	currentCommandScript = files[i];
-  	//var definition = require('./commands/' + files[i]);
   	exports.loadScript('./modules/' + files[i]);
   	console.log('[info] Command script loaded: ' + files[i]);
 }
 
 fs.readFile('./data/notify.json', handleFile)
 
-// Write the callback function
 function handleFile(err, data) {
     if (err) throw err
     obj = JSON.parse(data)
@@ -155,27 +165,75 @@ function handleFile(err, data) {
 }
 
 
+function autoKickUser(server, user) {
+	if(contains(exports.autokick, user.id)) {
+		bot.sendMessage(user, "Sorry, " + user + ", but you're not allow into " + server.name);
+		setTimeout(function() { bot.kickMember(server, user); }, 1000);
+	}
+}
+
+bot.on("serverNewMember", function(server, user) {
+	if(server.id == "135877399391764480") {
+		autoKickUser(server, user);
+	}
+});
+
+bot.on("ready", function() {
+	if(exports.mode == 0) {
+		bot.setStatus("online", "in the pond");
+	} else if(exports.mode == 1) {
+		bot.setStatus("away", "updating...");
+	} else if(exports.mode == 2) {
+		bot.setStatus("away", "maintance mode...");
+	} else if(exports.mode == 3) {
+		bot.setStatus("away", "fatal error!");
+	}
+});
+
+function notOnline(message) {
+	if(exports.mode == 1 || exports.mode == 2 || exports.mode == 3) {
+		var msg;
+		if(exports.mode == 1) {
+			msg = "Sorry, " + message.author + "; I'm currently being upgraded! I'll be back shortly with new awesome features!";
+		} else if (exports.mode == 2) {
+			msg = "Sorry, " + message.author + "; I'm currently under maintance! Some changes are being made to fix me up!"
+		} else if (exports.mode == 3) {
+			msg = "Oopsy! A fatal error has occoured somewhere! To keep my data safe, I'm no longer running any commands and have entered a lock state.";
+		}
+		if(message.channel instanceof Discord.PMChannel) {
+			bot.sendMessage(message, msg);
+		} else if (contains(message.mentions, bot.user)) {
+			bot.sendMessage(message, msg);
+		}
+	}
+	if(isMaster(message.author.id)) {
+		if(exports.mode != 3) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function processMentions(users, user) {
+	for(var u in users) {
+		var id = "<@" + users[u].id + ">";
+		if(id == user) {
+			return true;
+		}
+	}
+	return false;
+}
+
 bot.on("message", function(message) {
 	exports.messagelog.push(message);
 	if(stopMyself(message.author.id)) {
 		return;
 	}
-	/*if(!isMaster(message.author.id)) {
+	if(notOnline(message)) {
 		return;
-	}*/
-	// TODO: Notify logger (need to figure out how to not spam users...)
-	/*if(message.mentions.length > 0) {
-		for(var i = 0; i < message.mentions.length; i++) {
-			for(var ii = 0; ii < exports.notifyusers.length; ii++) {
-				if(message.mentions[i].id == exports.notifyusers[ii]) {
-					bot.sendMessage(message.mentions[i], message.content);
-					return;
-				}
-			}
-		}
-	}*/
+	}
+	
 	var msg = message.content.toLowerCase();
-
 	for (var i in exports.triggers) {
 		var trigger = exports.triggers[i];
 		if(trigger == msg) {
@@ -188,170 +246,83 @@ bot.on("message", function(message) {
 			}
 		}
 	}
-	main_loop:
-	for (var i in exports.triggers) {
-		var trigger = exports.triggers[i];
-		var args = [];
-		var ts = trigger.split(" ");
-		var ms = msg.split(" ");
-		for (var msk in ms) {
-			if(ms[msk] == "" || ms[msk] == " ") {
-				ms.splice(msk, 1);
-			}
-		}
-		var inMentions = false;
-		var inOptional = false;
-		var skipUntil = false;
-		var nm = [];
-		for (var t in ts) {
 
-			if (skipUntil != false) {
-				skipUntil = skipUntil - 1;
-			} else if(ts[t] == ms[t]) {
-				if(inMentions) {
-					/*if(!message.mentions.indexOf(ms[t]) > 0) {
-						nm.push(ts[t]);
-					}*/
+	var msg 	= message.content.toLowerCase();
+	var mst 	= msg.split(" ");
+	var ms 		= [];
+	for(var m in mst) {
+		if(mst[m] == "" || mst[m] == " ") {
+			continue;
+		}
+		ms.push(mst[m]);
+	}
+	if(!ms || ms.length == 0) {
+		return;
+	}
+	for(var tr in exports.triggers) {
+		var trigger    = exports.triggers[tr];
+		var ts     	   = trigger.split(" ");
+
+		var cmd    	   = [];
+		var args   	   = [];
+
+		var inArgs 	   = false;	
+		var inMentions = false;
+
+		for(var t in ts) {
+			if(inArgs == true) {
+				args.push(ms[t]);
+			} else if(inMentions == true) {
+				if(message.isMentioned(ms[t])) {
+					args.push(ms[t]);
 					continue;
 				} else {
-					nm.push(ts[t]);
+					inMentions = false;
 				}
-			} else if (inOptional) {
-				if(ts[t].endsWith("]")) {
-					/*var nosquare = ts[t].replace("]", "");
-					nm.push("]");*/
-					nm.push(ts[t]);
-					inOptional = false;
-				} else {
-					nm.push(ts[t]);
+			}
+			if(ts[t] == ms[t]) {
+				cmd.push(ts[t]);
+			} else if(ts[t] == "%arg%") {
+				cmd.push(ts[t]);
+				args.push(ms[t]);
+			} else if(ts[t] == "%args%") {
+				cmd.push(ts[t]);
+				args.push(ms[t]);
+				inArgs = true;
+			} else if(ts[t] == "%mention%") {
+				if(processMentions(message.mentions, ms[t])) {
+					cmd.push(ts[t]);
+					args.push(ms[t]);
 				}
-			} else if (ts[t] == "%arg%") {
-				nm.push(ts[t]);
-				args.push(ms[t]);
-			} else if (ts[t] == "%args%") {
-				nm.push(ts[t]);
-				if(ms.length > t) {
-					for(var msss in ms) {
-						args.push(ms[t++]);
-					}
-				}
-				args.push(ms[t]);
-				break;
-			} else if (ts[t] == "%mention%") {
-				/*if(message.mentions.indexOf(ms[t]) > 0) {
-					nm.push(ts[t]);
-				}*/
-				// Just going to push it straight up
-				nm.push(ts[t]);
-				args.push(ms[t]);
-			} else if (ts[t] == "%mentions%") {
-				/*if(message.mentions.indexOf(ms[t]) > 0) {
-					nm.push(ts[t]);
-				}*/
+			} else if(ts[t] == "%mentions%") {
 				inMentions = true;
-				// Just going to push it straight up
-				nm.push(ts[t]);
-				args.push(ms[t]);
-			} else if (ts[t] == "%integer%") {
-				if (isInt(ms[t])) {
-				    nm.push(ts[t]);
-				    args.push(ms[t]);
+				if(processMentions(message.mentions, ms[t])) {
+					cmd.push(ts[t]);
+					args.push(ms[t]);
 				}
-			} else if (ts[t].startsWith("[")) {
-				// hello [ducky dear]
+			} else if(ts[t] == "%integer%" || ts[t] == "%int%") {
+				if(isInt(ms[t])) {
+					cmd.push(ts[t]);
+					args.push(ms[t]);
+				} else {
+					cmd.push(ts[t]);
+				}
+			} else if(ts[t].startsWith("[")) {
 				if(ts[t].endsWith("]")) {
-					/*var nosquare = ts[t].replace("[", "");
-					var nosquare = ts[t].replace("]", "");
-					nm.push(nosquare);*/
-					nm.push(ts[t]);
-				} else {
-					/*var nosquare = ts[t].replace("[", "");
-					nm.push(nosquare);*/
-					nm.push(ts[t]);
-					inOptional = true;
-				}
-			} else if (ts[t].endsWith("]")) {
-				inOptional = false;
-				nm.push(ts[t]);
-				/*var nosquare = ts[t].replace("]", "");
-				nm.push(nosquare);*/
-			} else if (ts[t].startsWith("(")) {
-				var nobracket = ts[t].replace("(", "");
-				if(ts[t].endsWith(")")) {
-					nobracket = ts[t].replace(")", "");
-					if(contains(ts[t], "|")) {
-						var pipesplit = ts[t].split("|");
-						for (var pslit in pipesplit) {
-							console.log(ms[t] + " -> " + pipesplit[pslit])
-							if(ms[t] == pipesplit[pslit]) {
-								nm.push(ts[t]);
-								args.push(pipesplit[pslit]);
-								break;
-							}
-						}
-					} else if (ms[t] == nobracket) {
-						nm.push(ts[t]);
-					}
-				} else {
-					// hello (ducky|my dear)
-					// hello
-					// * (ducky|my
-					// dear)
+					var swb = ts[t];
+					swb = swb.replace("[" , "");
+					swb = swb.replace("]" , "");
 
-					// * ducky|my
-					if(contains(nobracket, "|")) {
-						var fnb = nobracket.split("|");
-						// ducky
-						// my
-						var poss = fnb[0];
-						if(ms[t] == poss) {
-							nm.push(ts[t]);
-							continue;
-						}
-					} else {
-						nm.push(ts[t]);
-					}
-					var fsplit = [];
-					var fcccc = 0;
-					for (var fslit in ts) {
-						var fccc = fcccc + t;
-						if(fccc > ts.length || !ts[fccc]) {
-							return;
-						}
-						var fc = fccc;
-						if(ts[fc].endsWith(")")) {
-							fsplit.push(ts[fc]);
-							break;
-						} else {
-							fsplit.push(ts[fc]);
-						}
-					}
-					if(fsplit.length == 0) {
-						var fspi = fsplit.join(" ");
-						var ufspi = fspi;
-						// (ducky|my dear|duck)
-						fspi = fspi.replace("(", "");
-						fspi = fspi.replace(")", "");
-						var fspiltt = fspi.split("|");
-						var someMatch = false;
-						for(var fspillt in fspiltt) {
-							if(fspiltt[fspillt] == ms[t]) {
-								someMatch = true;
-							}
-						}
-						if(someMatch == true) {
-							nm.push(ufspi);
-						}
-					} else {
-						return;
+					if(swb == ms[t]) {
+						cmd.push(ts[t]);
 					}
 				}
 			}
 		}
-		var newmsg = nm.join(" ");
-		if(trigger == newmsg) {
-			if(isMasterCMD(i, message.author.id)) {
-				var callback_function = exports.callbacks[i];
+		var command = cmd.join(" ");
+		if(trigger == command) {
+			if(isMasterCMD(tr, message.author.id)) {
+				var callback_function = exports.callbacks[tr];
  				callback_function(bot, message, msg, args);
  				return;
 			} else {
@@ -359,7 +330,6 @@ bot.on("message", function(message) {
 			}
  		}
 	}
-
 	return;
 });
 
